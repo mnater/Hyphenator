69ef17270f7b93c764a0138f55565dc241005f3c
mathias@mnn.ch <mathias@mnn.ch@09f05d58-b542-0410-874a-7da6d9b49ce3>
2008-10-27 16:27:59 +0000

    Lot of changes: - load patterns as space-separated-values and convert them to an object - added an on/off-switch - minor fixes

--- trunk/Hyphenator.js
+++ trunk/Hyphenator.js
@@ -15,7 +15,7 @@
 // Siehe http://creativecommons.org/licenses/by-sa/2.5/ch/
 // Somit sind sie frei, das Script für kommerzielle Zwecke zu nutzen
 //
-// Mathias Nater, Zürich, 2007
+// Mathias Nater, Zürich, 2008
 // mnater at mac dot com
 /**************** Preamble ****************/
 var Hyphenator=(function(){
@@ -38,11 +38,13 @@ var Hyphenator=(function(){
 				return t.substring(0,p);
 			}
 		}
-		return 'http://hyphenator.googlecode.com/svn/trunk/';
+		return 'http://127.0.0.1/~mnater/newtrunk/';
+		//return 'http://hyphenator.googlecode.com/svn/trunk/';
 	}();
 	var DONTHYPHENATE={'script':true,'code':true,'pre':true,'img':true,'br':true,'samp':true,'kbd':true,'var':true,'abbr':true,'acronym':true,'sub':true,'sup':true,'button':true,'option':true,'label':true};
 	var hyphenation={};
 	var enableRemoteLoading=true;
+	var displayToggleBox=false;
 	var hyphenateclass='hyphenate'; // the CSS-Classname of Elements that should be hyphenated eg. <p class="hyphenate">Text</p>
 	var hyphen=String.fromCharCode(173); // the hyphen, defaults to &shy; Change by Hyphenator.setHyphenChar(c);
 	var urlhyphen=_createZeroWidthSpace(); // the hyphe for urls, defaults to zerowidthspace; Change by Hyphenator.setUrlHyphenChar(c);
@@ -69,7 +71,7 @@ var Hyphenator=(function(){
 			zerowidthspace='';
 		}
 		return zerowidthspace;
-	}
+	};
 	
 	// checks if the script runs as a Bookmarklet
 	function _checkIfBookmarklet() {
@@ -95,7 +97,7 @@ var Hyphenator=(function(){
 	   }  else {
 	       //alert(msg);
 	   }
-	}
+	};
 		
 	//private methods	
 	/************ Language related methods ************/
@@ -157,6 +159,35 @@ var Hyphenator=(function(){
 			}
 		}
 	};
+	
+	function _switchToggleBox(s) {
+		if(s) {
+			var bdy=document.getElementsByTagName('body')[0];
+			var myBox=document.createElement('div');
+			var myIdAttribute=document.createAttribute('id');
+			myIdAttribute.nodeValue='HyphenatorToggleBox';
+			var myTextNode=document.createTextNode('Hy-phe-na-ti-on');
+			myBox.appendChild(myTextNode);
+			myBox.setAttributeNode(myIdAttribute);
+			myBox.onclick=Hyphenator.toggleHyphenation;
+			myBox.style.position='absolute';
+			myBox.style.top='0px';
+			myBox.style.right='0px';
+			myBox.style.margin='0';
+			myBox.style.backgroundColor='#AAAAAA';
+			myBox.style.color='#FFFFFF';
+			myBox.style.font='6pt Arial';
+			myBox.style.letterSpacing='0.2em';
+			myBox.style.padding='3px';
+			myBox.style.cursor='pointer';
+			myBox.style.WebkitBorderBottomLeftRadius='4px';
+			myBox.style.MozBorderRadiusBottomleft='4px';
+			bdy.appendChild(myBox);
+		} else {
+			var myBox=document.getElementById('HyphenatorToggleBox');
+			myBox.style.visibility='hidden';
+		}
+	};
 
     // gets the lang for the given Element
     // if not set, use the mainlanguage of the hole site
@@ -191,9 +222,6 @@ var Hyphenator=(function(){
 			_log("load patterns "+lang);
 		if(SUPPORTEDLANG[lang] && !patternsloaded[lang]) {
 	        var url=BASEPATH+'patterns/'+lang+'.js';
-	        if(lang=="de") {
-	        	url=BASEPATH+'patterns/newpatterns/de.js';
-	        }
 		} else {
 			return;
 		}
@@ -206,6 +234,20 @@ var Hyphenator=(function(){
 		}
 		if(DEBUG)
 			_log('Loading '+url);
+	};
+	
+	/* Converts from string '_a6' to object '_a':'_a6' */
+	function _convertPatternsToObject() {
+		for(var lang in Hyphenator.patterns) {
+			var sa=Hyphenator.patterns[lang].split(' ');
+			Hyphenator.patterns[lang]={};
+			var pat, key, i=0;
+			var isdigit=false;
+			while(pat=sa[i++]) {
+				key=pat.replace(/\d/g, '');
+				Hyphenator.patterns[lang][key]=pat;
+			}
+		}
 	};
 
 	/************ hyphenate helper methods ************/
@@ -232,52 +274,133 @@ var Hyphenator=(function(){
 			}
 		}
 	};
-	
-	//Run automatically when the DOM is loaded
-	/*  
-	 *  Code by Stuart Langridge
-	 *  http://www.kryogenix.org/days/2007/09/26/shortloaded
-	 *  based on code by Dean Edwards and John Resig
-	 *  http://dean.edwards.name/weblog/2006/06/again/
-	 *	http://javascript.nwbox.com/ContentLoaded/
-	 *
-	 */
-	function _runOnContentLoaded() {
-		(function(i) {
-			var u =navigator.userAgent;
-			var e=/*@cc_on!@*/false;
-			var st =setTimeout;
-			if(/webkit/i.test(u)) {
-				st(function(){
-						var dr=document.readyState;
-						if(dr=="loaded"||dr=="complete") {
-							i()
-						} else {
-							st(arguments.callee,10);
-						}
-					},
-				10);
-			}
-			else if((/mozilla/i.test(u)&&!/(compati)/.test(u)) || (/opera/i.test(u))) {
-				document.addEventListener("DOMContentLoaded",i,false);
-			} else if(e) {
-				(function(){
-					var t=document.createElement('doc:rdy');
-					try{
-						t.doScroll('left');
-						i();
-						t=null;
-					} catch(e) {
-						st(arguments.callee,0);
-					}
-				})();
+	function _removeHyphenation() {
+		var body=document.getElementsByTagName('body')[0];
+		if(Hyphenator.isBookmarklet()) {
+			Hyphenator.deleteHyphenationInElement(body);
+		} else {
+			if(document.getElementsByClassName) {
+				var elements=document.getElementsByClassName(hyphenateclass);
+				for(var i=0, l=elements.length; i<l; i++)
+				{
+					Hyphenator.deleteHyphenationInElement(elements[i]);
+				}
 			} else {
-				window.onload=i;
+				var elements=body.getElementsByTagName('*');
+				for(var i=0, l=elements.length; i<l; i++)
+				{
+					if(elements[i].className.indexOf(hyphenateclass)!=-1) {
+						Hyphenator.deleteHyphenationInElement(elements[i]);
+					}
+				}
 			}
-		})(function(){
-			_hideInside();
-			Hyphenator.hyphenateDocument();
-		});
+		}
+	};
+
+	
+	/*
+	 * ContentLoaded.js
+	 *
+	 * Author: Diego Perini (diego.perini at gmail.com)
+	 * Summary: Cross-browser wrapper for DOMContentLoaded
+	 * Updated: 17/05/2008
+	 * License: MIT
+	 * Version: 1.1
+	 *
+	 * URL:
+	 * http://javascript.nwbox.com/ContentLoaded/
+	 * http://javascript.nwbox.com/ContentLoaded/MIT-LICENSE
+	 *
+	 * Notes:
+	 * based on code by Dean Edwards and John Resig
+	 * http://dean.edwards.name/weblog/2006/06/again/
+	 */
+	// @w	window reference
+	// @f	function reference
+	function _runOnContentLoaded(w, f) {
+	
+		var	d = w.document,
+			D = 'DOMContentLoaded',
+			// user agent, version
+			u = w.navigator.userAgent.toLowerCase(),
+			v = parseFloat(u.match(/.+(?:rv|it|ml|ra|ie)[\/: ]([\d.]+)/)[1]);
+	
+		function init(e) {
+			if (!document.loaded) {
+				document.loaded = true;
+				// pass a fake event if needed
+				f((e.type && e.type == D) ? e : {
+					type: D,
+					target: d,
+					eventPhase: 0,
+					currentTarget: d,
+					timeStamp: +new Date,
+					eventType: e.type || e
+				});
+			}
+		}
+	
+		// safari < 525.13
+		if (/webkit\//.test(u) && v < 525.13) {
+	
+			(function () {
+				if (/complete|loaded/.test(d.readyState)) {
+					init('khtml-poll');
+				} else {
+					setTimeout(arguments.callee, 10);
+				}
+			})();
+	
+		// internet explorer all versions
+		} else if (/msie/.test(u) && !w.opera) {
+	
+			d.attachEvent('onreadystatechange',
+				function (e) {
+					if (d.readyState == 'complete') {
+						d.detachEvent('on'+e.type, arguments.callee);
+						init(e);
+					}
+				}
+			);
+			if (w == top) {
+				(function () {
+					try {
+						d.documentElement.doScroll('left');
+					} catch (e) {
+						setTimeout(arguments.callee, 10);
+						return;
+					}
+					init('msie-poll');
+				})();
+			}
+	
+		// browsers having native DOMContentLoaded
+		} else if (d.addEventListener &&
+			(/opera\//.test(u) && v > 9) ||
+			(/gecko\//.test(u) && v >= 1.8) ||
+			(/khtml\//.test(u) && v >= 4.0) ||
+			(/webkit\//.test(u) && v >= 525.13)) {
+	
+			d.addEventListener(D,
+				function (e) {
+					d.removeEventListener(D, arguments.callee, false);
+					init(e);
+				}, false
+			);
+	
+		// fallback to last resort for older browsers
+		} else {
+	
+			// from Simon Willison
+			var oldonload = w.onload;
+			w.onload = function (e) {
+				init(e || w.event);
+				if (typeof oldonload == 'function') {
+					oldonload(e || w.event);
+				}
+			};
+	
+		}
 	};
 
 
@@ -302,7 +425,13 @@ var Hyphenator=(function(){
 		
 		//public methods
 		run: function() {
-			_runOnContentLoaded();
+			_runOnContentLoaded(window, function(){
+				_hideInside();
+				Hyphenator.hyphenateDocument();
+				if(displayToggleBox) {
+					_switchToggleBox(true);
+				}
+			});
 		},
 		addExceptions: function(words) { //words is a comma separated string of words
 		 	var w=words.split(',');
@@ -320,7 +449,13 @@ var Hyphenator=(function(){
             min=mymin || 6;
 		},
 		setHyphenChar: function(str) {
+			if(str=='&shy;') {
+				str=String.fromCharCode(173);
+			}
             hyphen=str || String.fromCharCode(173);
+		},
+		setDisplayToggleBox: function(bool) {
+            displayToggleBox=bool || true;
 		},
 		setUrlHyphenChar: function(str) {
             urlhyphen=str || _createZeroWidthSpace();
@@ -393,6 +528,7 @@ var Hyphenator=(function(){
 				var interval=window.setInterval(function(){
 					if(preparestate==2) {
 						window.clearInterval(interval);
+						_convertPatternsToObject();
 						_runHyphenation();
 					}
 				},10);
@@ -452,6 +588,29 @@ var Hyphenator=(function(){
 	            el.style.visibility='visible';
 	        }
         },
+        deleteHyphenationInElement : function(el) {
+        	var h;
+        	switch(hyphen) {
+        		case '|':
+        			h='\\|';
+        			break;
+        		case '+':
+					h='\\+';
+					break;
+				case '*':
+					h='\\*'
+					break;
+				default:
+				h=hyphen;
+        	}
+        	for(var i=0; (n=el.childNodes[i]); i++) {
+        		if(n.nodeType==3) {
+        			n.data=n.data.replace(new RegExp(h,'g'), '');
+        		} else if(n.nodeType==1) {
+        			Hyphenator.deleteHyphenationInElement(n);
+        		}
+        	}
+        },
 		hyphenateWord    : function(lang,word) {
 			if(word=='') {
 				return '';
@@ -460,10 +619,7 @@ var Hyphenator=(function(){
 				//word already contains shy; -> leave at it is!
 				return word;
 			}
-			if(hyphen=='&shy;') {
-				hyphen=String.fromCharCode(173);
-			}
-			if(hyphenation[word]) {
+			if(hyphenation[word]) { //the word is in the exceptions list
 				return hyphenation[word].replace(/-/g,hyphen);
 			}
 			if(word.indexOf('-')!=-1) {
@@ -475,47 +631,62 @@ var Hyphenator=(function(){
 				return parts.join('-'+zerowidthspace);
 			}
 			//finally the core hyphenation algorithm
-			var positions = []; 		//hyphenating points
-			var result = [];			//syllabs
-			var w='_'+word.toLowerCase()+'_';	//mark beginning an end
+			var w='_'+word+'_';
 			var wl=w.length;
-			var i=wl-2;
-			do {
-				positions[i]=0;
-			} while(i--);
-			var s=wl-1;
-			do {
-				var maxl=wl-s;
-				var window=w.substring(s);
-				for(var l=Hyphenator.shortestPattern[lang]; l<=maxl && l<=Hyphenator.longestPattern[lang]; l++) {
-					var part=window.substring(0,l);	//window from position s with length l
-					var values=null;
-					if(Hyphenator.patterns[lang][part]!==undefined) {
-						values=Hyphenator.patterns[lang][part];
-						var i=s-1;
-						var v;
-						for(var p=0, le=values.length; p<le; p++, i++) {
-							v=parseInt(values.charAt(p));
-							if(v>positions[i]) {
-								positions[i]=v; //set the values, overwriting lower values
+			var s=w.split('');
+			w=w.toLowerCase();
+			var hypos=new Array();
+			var p, maxwins, win, pat, patl, i, c, digits, z;
+			var numb3rs={'0':true,'1':true,'2':true,'3':true,'4':true,'5':true,'6':true,'7':true,'8':true,'9':true}; //check for member is faster then isFinite()
+			for(p=0; p<=(wl-Hyphenator.shortestPattern[lang]); p++) {
+				maxwins=Math.min((wl-p),Hyphenator.longestPattern[lang]);
+				for(win=Hyphenator.shortestPattern[lang]; win<=maxwins; win++) {
+					if(pat=Hyphenator.patterns[lang][w.substr(p,win)]) {
+						digits=1;
+						patl=pat.length;
+						for(i=0; i<patl; i++) {
+							c=pat.charAt(i);
+							if(numb3rs[c]) {
+								if(i==0) {
+									z=p-1;
+									if(!hypos[z] || hypos[z]<c) {
+										hypos[z]=c;
+									}
+								} else {
+									z=p+i-digits;
+									if(!hypos[z] || hypos[z]<c) {
+										hypos[z]=c;
+									}
+								}
+								digits++;								
 							}
 						}
 					}
 				}
-			} while(s--)
-			wl=word.length;
-			for(i=1; i<wl; i++) {
-				if(!!(positions[i]&1) && i>=Hyphenator.leftmin[lang] && i<=word.length-Hyphenator.rightmin[lang]) {
-					result.push(word.substring(result.join('').length,i)); //Silben eintragen
+			}
+			var inserted=0;
+			for(i=Hyphenator.leftmin[lang]; i<(hypos.length-Hyphenator.rightmin[lang]); i++) {
+				if(!!(hypos[i]&1)) {
+					s.splice(i+inserted+1,0,hyphen);
+					inserted++;
 				}
 			}
-			result.push(word.substring(result.join('').length,i)); //Letzte Silbe eintragen
-			return result.join(hyphen);
+			return s.slice(1,-1).join('');
 		},
-		hyphenateURL: function(url){
+		hyphenateURL: function(url) {
 			return url.replace(/([:\/\.\?#&_,;!@]+)/gi,'$&'+urlhyphen);
+		},
+		toggleHyphenation: function() {
+			var currentText=document.getElementById('HyphenatorToggleBox').firstChild.nodeValue;
+			if(currentText=='Hy-phe-na-ti-on') {
+				_removeHyphenation();
+				document.getElementById('HyphenatorToggleBox').firstChild.nodeValue='Hyphenation';
+				
+			} else {
+				_runHyphenation();
+				document.getElementById('HyphenatorToggleBox').firstChild.nodeValue='Hy-phe-na-ti-on';
+			}
 		}
-
 	};
 })();
 if(Hyphenator.isBookmarklet()) {
