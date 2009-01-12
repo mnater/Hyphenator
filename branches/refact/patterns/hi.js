16b49650336e323affe8193aea31264cee1311a2
mathias@mnn.ch <mathias@mnn.ch@09f05d58-b542-0410-874a-7da6d9b49ce3>
2009-01-12 17:00:04 +0000

    Created a refactoring branch

---
+++ branches/refact/patterns/hi.js
@@ -0,0 +1,9 @@
+// For questions about the Hindi hyphenation patterns
+// ask Santhosh Thottingal (santhosh dot thottingal at gmail dot com)
+Hyphenator.leftmin['hi']=2;
+Hyphenator.rightmin['hi']=2;
+Hyphenator.shortestPattern['hi']=1;
+Hyphenator.longestPattern['hi']=3;
+Hyphenator.specialChars['hi']='आअइईउऊऋऎएऐऒऔकगखघङचछजझञटठडढणतथदधनपफबभमयरलवशषसहळऴऱिीाुूृॆेॊाोैौ्ःं‍';
+Hyphenator.patterns['hi']="अ1 आ1 इ1 ई1 उ1 ऊ1 ऋ1 ऎ1 ए1 ऐ1 ऒ1 औ1 ि1 ा1 ी1 ु1 ू1 ृ1 ॆ1 े1 ॊ1 ो1 ौ1 ्2 ः1 ं1 2न्‍ 2र्‍ 2ल्‍ 2ळ्‍ 2ण्‍ 1क 1ग 1ख 1घ 1ङ 1च 1छ 1ज 1झ 1ञ 1ट 1ठ 1ड 1ढ 1ण 1त 1थ 1द 1ध 1न 1प 1फ 1ब 1भ 1म 1य 1र 1ल 1व 1श 1ष 1स 1ह 1ळ 1ऴ 1ऱ";
+Hyphenator.updatePatternsLoadState('hi',true);
---
+++ branches/refact/patterns/ta.js
@@ -0,0 +1,9 @@
+// For questions about the Tamil hyphenation patterns
+// ask Santhosh Thottingal (santhosh dot thottingal at gmail dot com)
+Hyphenator.leftmin['ta']=2;
+Hyphenator.rightmin['ta']=2;
+Hyphenator.shortestPattern['ta']=1;
+Hyphenator.longestPattern['ta']=1;
+Hyphenator.specialChars['ta']='ஆஅஇஈஉஊஎஏஐஒஔகஙசஜஞடணதநபமயரலவஶஷஸஹளழறிீாுூெேொாோைௌௗ்ஃஂ‍';
+Hyphenator.patterns['ta']="அ1 ஆ1 இ1 ஈ1 உ1 ஊ1 எ1 ஏ1 ஐ1 ஒ1 ஔ1 ி1 ா1 ீ1 ு1 ூ1 ெ1 ே1 ொ1 ோ1 ௌ1 ௗ1 ்2 ஃ1 ஂ1 1க 1ங 1ச 1ஜ 1ஞ 1ட 1ண 1த 1ந 1ப 1ம 1ய 1ர 1ல 1வ 1ஶ 1ஷ 1ஸ 1ஹ 1ள 1ழ 1ற";
+Hyphenator.updatePatternsLoadState('ta',true);
