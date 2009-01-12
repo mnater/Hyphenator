f8e28ccb63b482fc1ff84e86a708a0b4bb383d45
mathias@mnn.ch <mathias@mnn.ch@09f05d58-b542-0410-874a-7da6d9b49ce3>
2009-01-12 16:24:15 +0000

    Tagged HyphenatorBeta12

---
+++ tags/HyphenatorBeta12/patterns/bn.js
@@ -0,0 +1,10 @@
+// For questions about the Bengali hyphenation patterns
+// ask Santhosh Thottingal (santhosh dot thottingal at gmail dot com)
+Hyphenator.leftmin['bn']=2;
+Hyphenator.rightmin['bn']=2;
+Hyphenator.shortestPattern['bn']=1;
+Hyphenator.longestPattern['bn']=3;
+Hyphenator.specialChars['bn']='আঅইঈউঊঋএঐঔকগখঘঙচছজঝঞটঠডঢণতথদধনপফবভমযরলশষসহিীাুূৃোোৈৌৗ্ঃং‍';
+Hyphenator.patterns['bn']="অ1 আ1 ই1 ঈ1 উ1 ঊ1 ঋ1 এ1 ঐ1 ঔ1 ি1 া1 ী1 ু1 ৃ1 ে1 ো1 ৌ1 ৗ1 ্2 ঃ1 ং1 2ন্‍ 2র্‍ 2ল্‍ 2্‍ 2ণ্‍ 1ক 1গ 1খ 1ঘ 1ঙ 1চ 1ছ 1জ 1ঝ 1ঞ 1ট 1ঠ 1ড 1ঢ 1ণ 1ত 1থ 1দ 1ধ 1ন 1প 1ফ 1ব 1ভ 1ম 1য 1র 1ল 1শ 1ষ 1স 1হ";
+Hyphenator.updatePatternsLoadState('bn',true);
+
