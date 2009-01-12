eb302c8f8b85989b1fdabac7fbcd6b379f9a5588
mathias@mnn.ch <mathias@mnn.ch@09f05d58-b542-0410-874a-7da6d9b49ce3>
2009-01-12 16:29:28 +0000

    delete faulty tag tags/HyphenatorBeta12

--- tags/HyphenatorBeta12/patterns/te.js
+++
@@ -1,9 +0,0 @@
-// For questions about the Telugu hyphenation patterns
-// ask Santhosh Thottingal (santhosh dot thottingal at gmail dot com)
-Hyphenator.leftmin['te']=2;
-Hyphenator.rightmin['te']=2;
-Hyphenator.shortestPattern['te']=1;
-Hyphenator.longestPattern['te']=3;
-Hyphenator.specialChars['te']='ఆఅఇఈఉఊఋఎఏఐఒఔకగఖఘఙచఛజఝఞటఠడఢణతథదధనపఫబభమయరలవశషసహళఱిీాుూృెేొాోైౌ్ఃం‍';
-Hyphenator.patterns['te']="అ1 ఆ1 ఇ1 ఈ1 ఉ1 ఊ1 ఋ1 ఎ1 ఏ1 ఐ1 ఒ1 ఔ1 ి1 ా1 ీ1 ు1 ూ1 ృ1 ె1 ే1 ొ1 ో1 ౌ1 ్2 ః1 ం1 2న్‍ 2ర్‍ 2ల్‍ 2ళ్‍ 2ణ్‍ 1క 1గ1 1ఖ 1ఘ 1ఙ 1చ 1ఛ 1జ 1ఝ 1ఞ 1ట 1ఠ 1డ 1ఢ 1ణ 1త 1థ 1ద 1ధ 1న 1ప 1ఫ 1బ 1భ 1మ 1య 1ర 1ల 1వ 1శ 1ష 1స 1హ 1ళ 1ఱ";
-Hyphenator.updatePatternsLoadState('te',true);
