147adc618836c6a356367e1c6e239b302f2d2e44
Mathias Nater <mathiasnater@gmail.com>
2009-04-10 16:37:47 +0000

    Reformatted license comment

--- Hyphenator.js
+++ Hyphenator.js
@@ -1,24 +1,26 @@
-﻿/**************** Preamble ****************/
-//  Hyphenator 2.X.X - client side hyphenation for webbrowsers
-//  Copyright (C) 2009  Mathias Nater, Zürich (mathias at mnn dot ch)
-//  Project and Source hosted on http://code.google.com/p/hyphenator/
-// 
-//  This JavaScript code is free software: you can redistribute
-//  it and/or modify it under the terms of the GNU Lesser
-//  General Public License (GNU LGPL) as published by the Free Software
-//  Foundation, either version 3 of the License, or (at your option)
-//  any later version.  The code is distributed WITHOUT ANY WARRANTY;
-//  without even the implied warranty of MERCHANTABILITY or FITNESS
-//  FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
-//
-//  As additional permission under GNU GPL version 3 section 7, you
-//  may distribute non-source (e.g., minimized or compacted) forms of
-//  that code without the copy of the GNU GPL normally required by
-//  section 4, provided you include this license notice and a URL
-//  through which recipients can access the Corresponding Source.
-//
-//  Comments are jsdoctoolkit formatted. See jsdoctoolkit.org
-/**************** Preamble ****************/
+﻿/*!
+ *  Hyphenator 2.X.X - client side hyphenation for webbrowsers
+ *  Copyright (C) 2009  Mathias Nater, Zürich (mathias at mnn dot ch)
+ *  Project and Source hosted on http: *code.google.com/p/hyphenator/
+ * 
+ *  This JavaScript code is free software: you can redistribute
+ *  it and/or modify it under the terms of the GNU Lesser
+ *  General Public License (GNU LGPL) as published by the Free Software
+ *  Foundation, either version 3 of the License, or (at your option)
+ *  any later version.  The code is distributed WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
+ *
+ *  As additional permission under GNU GPL version 3 section 7, you
+ *  may distribute non-source (e.g., minimized or compacted) forms of
+ *  that code without the copy of the GNU GPL normally required by
+ *  section 4, provided you include this license notice and a URL
+ *  through which recipients can access the Corresponding Source.
+ */
+ 
+/* 
+ *  Comments are jsdoctoolkit formatted. See jsdoctoolkit.org
+ */
     
 /**
  * @fileOverview
