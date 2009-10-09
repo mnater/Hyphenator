86ee20a434b9a46a1f8b924e3814db4c1b7eb514
mathias@mnn.ch <mathias@mnn.ch@09f05d58-b542-0410-874a-7da6d9b49ce3>
2009-10-09 20:56:03 +0000

    - removed registeroncopy-code (is in sep branch) - updated version number

--- trunk/Hyphenator.js
+++ trunk/Hyphenator.js
@@ -761,53 +761,7 @@ var Hyphenator = (function () {
 		if (elements.length > 0) {
 			Expando.appendDataForElem(elements[elements.length - 1], {isLast : true});
 		}
-	},
-	
-/*	registerOnCopy = function () {
-			document.getElementsByTagName('body')[0].oncopy = function (e) {
-				var text, h;
-				var myArea = document.createElement('textarea');
-				var myStyleAttribute = document.createAttribute('style');
-				myStyleAttribute.nodeValue = 'display:block';
-				myArea.setAttributeNode(myStyleAttribute);
-				document.getElementsByTagName('body')[0].appendChild(myArea);
-				if (window.getSelection) {
-					myArea.value = window.getSelection();
-					text = window.getSelection().toString();
-				}
-				else if (document.selection) { // should come last; Opera!
-					text = document.selection.createRange().text;
-				}
-				switch (hyphen) {
-					case '|':
-						h = '\\|';
-						break;
-					case '+':
-						h = '\\+';
-						break;
-					case '*':
-						h = '\\*';
-						break;
-					case String.fromCharCode(173):
-						h = '\u00AD';
-						break;
-					default:
-						h = hyphen;
-					}
-				text = text.replace(new RegExp(h, 'g'), '');
-				text = text.replace(new RegExp(zeroWidthSpace, 'g'), '');
-				alert(text);
-				if (!!e && !!e.clipboardData) { //Safari
-					e.preventDefault();
-					e.clipboardData.setData('text/plain', text);
-				} else if (!!window.clipboardData) { // IE
-					window.preventDefault();
-					window.clipboardData.setData('Text', text);
-				}
-			}			
-	},
-*/
-	
+	},	
 	 
 	/**
 	 * @name Hyphenator-convertPatterns
@@ -1452,7 +1406,6 @@ var Hyphenator = (function () {
 					if (displayToggleBox) {
 						toggleBox(true);
 					}
-					//registerOnCopy();
 				} catch (e) {
 					onError(e);
 				}
