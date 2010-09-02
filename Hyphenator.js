9c9897bd9bffb002bae12b5d2076455e3e6e159f
Mathias Nater <mathiasnater@gmail.com>
2010-09-02 18:23:59 +0000

    Set version

--- Hyphenator.js
+++ Hyphenator.js
@@ -1,4 +1,4 @@
-﻿/** @license Hyphenator X.Y.Z - client side hyphenation for webbrowsers
+﻿/** @license Hyphenator 3.1.0 - client side hyphenation for webbrowsers
  *  Copyright (C) 2010  Mathias Nater, Zürich (mathias at mnn dot ch)
  *  Project and Source hosted on http://code.google.com/p/hyphenator/
  * 
@@ -31,7 +31,7 @@
  * @description Provides all functionality to do hyphenation, except the patterns that are loaded
  * externally.
  * @author Mathias Nater, <a href = "mailto:mathias@mnn.ch">mathias@mnn.ch</a>
- * @version X.Y.Z
+ * @version 3.1.0
  * @namespace Holds all methods and properties
  * @example
  * &lt;script src = "Hyphenator.js" type = "text/javascript"&gt;&lt;/script&gt;
@@ -1569,7 +1569,7 @@ var Hyphenator = (function (window) {
 		 * minor release: new languages, improvements
 		 * @public
          */		
-		version: 'X.Y.Z',
+		version: '3.1.0',
 		
 		/**
 		 * @name Hyphenator.languages
