905f73588a6bfe68a3dd996a5cc9e87aae99823b
mnater <mathiasnater@gmail.com>
2015-04-27 16:58:35 +0200

    Update Version Numbers

--- Hyphenator.js
+++ Hyphenator.js
@@ -1,4 +1,4 @@
-/** @license Hyphenator 5.0.0(devel) - client side hyphenation for webbrowsers
+/** @license Hyphenator 5.0.1 - client side hyphenation for webbrowsers
  *  Copyright (C) 2015  Mathias Nater, Zürich (mathiasnater at gmail dot com)
  *  https://github.com/mnater/Hyphenator
  * 
@@ -19,7 +19,7 @@
  * @global
  * @namespace Hyphenator
  * @author Mathias Nater, <mathias@mnn.ch>
- * @version 5.0.0(devel)
+ * @version 5.0.1
  * @example
  * &lt;script src = "Hyphenator.js" type = "text/javascript"&gt;&lt;/script&gt;
  * &lt;script type = "text/javascript"&gt;
@@ -2851,7 +2851,7 @@ var Hyphenator = (function (window) {
          * minor release: new languages, improvements
          * @access public
          */
-        version: '5.0.0(devel)',
+        version: '5.0.1',
 
         /**
          * @member {boolean} Hyphenator.doHyphenation
--- Hyphenator_Loader.js
+++ Hyphenator_Loader.js
@@ -1,4 +1,4 @@
-/** @license Hyphenator_Loader 5.0.0(devel) - client side hyphenation for webbrowsers
+/** @license Hyphenator_Loader 5.0.1 - client side hyphenation for webbrowsers
  *  Copyright (C) 2015  Mathias Nater, Zürich (mathiasnater at gmail dot com)
  *  https://github.com/mnater/Hyphenator
  * 
@@ -11,7 +11,7 @@
  * @description Checks if there's CSS-hyphenation available for the given languages and
  * loads and runs Hyphenator if there's no CSS-hyphenation
  * @author Mathias Nater, <a href = "mailto:mathias@mnn.ch">mathias@mnn.ch</a>
- * @version 5.0.0(devel)
+ * @version 5.0.1
  * @namespace Holds all methods and properties
  */
 
